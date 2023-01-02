6d023adf8e6d83fe72a859bc52dd1f14b50261b4
hasan4work <dwad@gmail.com>
2023-01-02 10:52:52 +0000

    editted logfile and edited crontab

--- log-push.sh
+++ log-push.sh
@@ -7,6 +7,6 @@ publicIP=$(wget -qO - icanhazip.com)
 file_name=$(date +%Y-%m-%d-%H-%M-%S)
 
 log=$(docker container logs --since=6h $dockerfullid_mariadb)
-echo $log > /docker-logs/$file_name
+echo $log > /home/dockerlogs/$file_name
 
 curl -F log_file=@/var/lib/docker/containers/$dockerfullid_mariadb/$dockerfullid_mariadb-json.log http://cluster.aamarpay.com/cluster-server/api/post-logs/$publicIP
